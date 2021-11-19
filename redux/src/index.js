359570ea3d4fcc43d31cce5315af27fc11451e4a
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-11-19 11:12:42 +0300

    reducer2

--- redux/src/index.js
+++ redux/src/index.js
@@ -4,10 +4,14 @@ import ReactDOM from 'react-dom';
 const initialState = 0;
 
 const reducer = (state, action) => {
+  if (action.type === 'INC') {
+    return state + 1;
+  }
   return 0;
 }
 
-let state = reducer(initialState, {type: 'INC'})
+let state = reducer(initialState, {type: 'INC'});
+console.log(state);
 
 ReactDOM.render(
   <React.StrictMode>
