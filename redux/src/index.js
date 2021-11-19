4c2c5f9672297efaa011cae0efddc2a42601d3b5
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-11-19 11:23:36 +0300

    reducer4

--- redux/src/index.js
+++ redux/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
 
-const initialState = 0;
+// const initialState = 0;
 
 const reducer = (state = 0, action) => {
   switch (action.type) {
@@ -12,10 +13,14 @@ const reducer = (state = 0, action) => {
   }
 }
 
-let state = reducer(initialState, {type: 'INC'});
-state = reducer(state, {type: 'INC'});
-state = reducer(state, {type: 'INC'});
-console.log(state);
+const store = createStore(reducer);
+
+console.log(store.getState());
+
+// let state = reducer(initialState, {type: 'INC'});
+// state = reducer(state, {type: 'INC'});
+// state = reducer(state, {type: 'INC'});
+// console.log(state);
 
 ReactDOM.render(
   <React.StrictMode>
