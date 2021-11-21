86b5dbc71caf76dbb0c1ebdaa7601267fe919615
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-11-21 13:31:47 +0300

    destruction

--- redux/src/index.js
+++ redux/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore, bindActionCreators} from 'redux';
 import reducer from './reducer';
-import { dec, inc, rnd } from './actions';
+import * as actions from './actions';
 
 const store = createStore(reducer);
 
@@ -18,19 +18,15 @@ subscribe(update);
 //   dispatch(creator(...args));
 // };
 
-const {incDispatch, decDispatch, rndDispatch} = bindActionCreators({
-  incDispatch: inc,
-  decDispatch: dec,
-  rndDispatch: rnd
-}, dispatch);
+const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
 
-document.getElementById('inc').addEventListener('click', incDispatch);
+document.getElementById('inc').addEventListener('click', inc);
 
-document.getElementById('dec').addEventListener('click', decDispatch);
+document.getElementById('dec').addEventListener('click', dec);
 
 document.getElementById('rnd').addEventListener('click', () => {
   const value = Math.floor(Math.random() * 10);
-  rndDispatch(value);
+  rnd(value);
 });
 
 
