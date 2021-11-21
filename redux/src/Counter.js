c8add9b9c44582eb693a0602404e3b26e075f850
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-11-21 13:55:44 +0300

    add React 2

--- redux/src/Counter.js
+++ redux/src/Counter.js
@@ -1,1 +1,12 @@
-const Counter = {};
+const Counter = ({counter, inc, dec, rnd}) => {
+    return (
+        <div>
+            <h1>{counter}</h1>
+            <button onClick={dec}>DEC</button>
+            <button onClick={inc}>INC</button>
+            <button onClick={rnd}>RND</button>
+        </div>
+    )
+};
+
+export default Counter;
--- redux/src/index.js
+++ redux/src/index.js
@@ -3,37 +3,29 @@ import ReactDOM from 'react-dom';
 import {createStore, bindActionCreators} from 'redux';
 import reducer from './reducer';
 import * as actions from './actions';
+import Counter from './Counter';
 
 const store = createStore(reducer);
 
 const {dispatch, subscribe, getState} = store;
-
-const update = () => {
-  document.getElementById('counter').textContent = getState().value;
-}
-
-subscribe(update);
-
-// const bindActionCreator = (creator, dispatch) => (...args) => {
-//   dispatch(creator(...args));
-// };
-
 const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
 
-document.getElementById('inc').addEventListener('click', inc);
-
-document.getElementById('dec').addEventListener('click', dec);
-
-document.getElementById('rnd').addEventListener('click', () => {
-  const value = Math.floor(Math.random() * 10);
-  rnd(value);
-});
-
-
-ReactDOM.render(
-  <React.StrictMode>
-    
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const update = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Counter
+        counter={getState().value}
+        inc={inc}
+        dec={dec}
+        rnd={() => {
+          const value = Math.floor(Math.random() * 10);
+          rnd(value);
+        }}
+      />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+update();
+subscribe(update);
 
