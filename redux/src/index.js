9c67521d90948a27cacc49a59e05b84fbb59c7c5
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-11-21 13:01:48 +0300

    actions

--- redux/src/index.js
+++ redux/src/index.js
@@ -1,77 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
-
-const initialState = {value: 0};
-
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case 'INC':
-      return {
-        ...state, 
-        value: state.value + 1
-      };
-    case 'DEC':
-      return {
-        ...state, 
-        value: state.value - 1
-      };
-    case 'RND':
-      return {
-        ...state, 
-        value: state.value * action.payload
-      };
-    default:
-      return state;
-  }
-}
+import reducer from './reducer';
+import { dec, inc, rnd } from './actions';
 
 const store = createStore(reducer);
 
+const {dispatch, subscribe, getState} = store;
+
 const update = () => {
-  document.getElementById('counter').textContent = store.getState().value;
+  document.getElementById('counter').textContent = getState().value;
 }
 
-store.subscribe(update);
-
-const inc = () => {
-  return {
-    type: 'INC'
-  }
-}
-
-const dec = () => {
-  return {
-    type: 'DEC'
-  }
-}
-
-const rnd = (value) => {
-  return {
-    type: 'RND',
-    payload: value
-  }
-}
+subscribe(update);
 
 document.getElementById('inc').addEventListener('click', () => {
-  store.dispatch(inc());
+  dispatch(inc());
 });
 
 document.getElementById('dec').addEventListener('click', () => {
-  store.dispatch(dec());
+  dispatch(dec());
 });
 
 document.getElementById('rnd').addEventListener('click', () => {
   const value = Math.floor(Math.random() * 10);
-  store.dispatch(rnd(value));
+  dispatch(rnd(value));
 });
 
-
-
-// let state = reducer(initialState, {type: 'INC'});
-// state = reducer(state, {type: 'INC'});
-// state = reducer(state, {type: 'INC'});
-// console.log(state);
 
 ReactDOM.render(
   <React.StrictMode>
--- redux/src/script.js
+++
@@ -1,4 +0,0 @@
-const sum = (a, b) => a + b;
-
-console.log(sum(5, 10));
-console.log(sum(5, 10));
